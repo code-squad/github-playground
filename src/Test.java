37a76c15c7737cb87abe32c0125a71a97690a470
95degree <95degree@naver.com>
2021-02-08 17:12:33 +0900

    add printRebaseMessage method
    rebase 메세지를 출력하는 메소드를 추가했습니다.

--- src/Test.java
+++ src/Test.java
@@ -2,4 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("실험용");
     }
+    public static void printRebaseMessage(){
+        System.out.println("rebase");
+    }
 }
